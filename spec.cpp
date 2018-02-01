ad79b5edc33964a33a1ef13a12707b5ac0b2a257
hxchi <981863673@qq.com>
2018-02-01 22:03:31 +0800

    creat event, with commands.

--- spec.cpp
+++ spec.cpp
@@ -8,9 +8,10 @@
 int main()
 {
 	// from root
-	TFile *prf = new TFile("~/sadata/out/totalspec.root", "recreate");
-	TCanvas *c = new TCanvas("c", " ", 600, 600);
-	TH1D *h = new TH1D("h", " ", 16384, 0, 16383);
+	TFile *prf = new TFile("r.root", "recreate");
+	TH1D *htime = new TH1D("htime", " ", 16000, 2000, 6000);
+	TH1D *hspec = new TH1D("hspec", " ", 4096, 0, 2047);
+	TH1D *hmultifired = new TH1D("hmultifired", " ", 8, 1, 9);
 
 	struct SARDATAHEADER // south africa data header
 	{
@@ -26,73 +27,243 @@ int main()
 	static unsigned short blocksize = 32768;
 
 
-	// for eff
-	double eff[9][4][3];
-	geteff(eff);
+	// for energy cal
+	double calpar[9][4][3];
+	getcalpar(calpar);
 
 	FILE *fp;
 
-	if ((fp=fopen("~/sadata/d17/R17_0", "rb")) == NULL) {
+	if ((fp=fopen("../d17/R17_0", "rb")) == NULL) {
 		printf("can not open the file.\n");
 		return -1;
 	}
 
-	unsigned short timestamp[2];
-	unsigned short adc = 0;
-	unsigned short tdc = 0;
+
+	// const value
+	const int coindiencetime = 150; // unit 10 ns 
+
+	unsigned short adc[9][4];
+	unsigned short tdc[9][4];
+	long long etime[9][4][2];
+	long long xtime[9][4][2];
 	unsigned short label_head;
 	unsigned short label_tail;
-	double ene = 0;
+	double addbackene = 0;
 	unsigned short block[blocksize];
 	unsigned short *pb;
 
+	// for event
+	const int timeciondience = 150;
+	int kk = 0;
+	bool eventflag = 1;
+	// int tmptimestampcount = 0;
+	long long tmptime = 0;
+ 	unsigned short tmptimestamp[2][2];
+ 	long long t0 = 0;
+ 	long long t1 = 0; // for time coindience
+ 	int firecount;
+
+ 	// for addback
+ 	double eaddback[9];
+ 	long long taddback[9];
 
 	while (fread(block, sizeof(unsigned short), blocksize, fp)) {
+		// fread(block, sizeof(unsigned short), blocksize, fp);
 		pb = block;
 		// jump block header
 		pb += sizeof(dataheader_)/2; 
 
-		pb += 3; // jump to the last unsignede short number in a fired
-		while (pb != (block + blocksize)) {
-			pb += 4;
-
-			// test if the end 
-			if (*pb == 0x5e5e && *(pb+1) == 0x5e5e && *(pb-1) == 0x5e5e) {
-				// cout << "end of a block..." << endl;
+		pb += 7; // jump to the last unsignede short number in a fired
+		// in r17_0, the first fired is c052(clover0 segement c), but without energy, only tdc, so jump this fired...
+		// what in other data file, still needs to be comfired...
+		while (pb != (block + blocksize)) { // while (pb != (block + blocksize)) // here is in each block...
+			if (*pb == 0x5e5e && *(pb+1) == 0x5e5e && *(pb-1) == 0x5e5e) { // test if the end of a block
 				break;
 			}
 
-			label_head = *pb >> 12;
-			label_tail = *pb & 0x0FFF;
+			// if (kk == 2) { // control event number
+			// 	break;
+			// }
 
-			if (label_head == 8) { // jump useless data
-				continue;
-			}
-			else if (label_head == 12) {
-				timestamp[0] = *(pb-3);
-				timestamp[1] = *(pb-2);
-				if ((((label_tail>>4) & 0x00F)%2) == 0) { // get the middle number of label_tail
-					adc = *(pb-1) & 0x3FFF; 
+			eventflag = 1;
+			while (eventflag) { // in an event
+				// init
+				// cout << "...start an event..." << endl;
+				firecount = 0;
+				// tmptimestampcount = 0;
+				for (int m = 0; m < 9; m++) {
+					for (int n = 0; n < 4; n++) {
+						adc[m][n] = 0;
+						tdc[m][n] = 0;
+					}
 				}
-				else {
-					tdc = *(pb-1) & 0x3FFF; 
+
+				for (int r = 0; r < 9; r++) {
+					for (int s = 0; s < 4; s++) {
+						for (int t = 0; t < 2; t++) {
+							etime[r][s][t] = 0;
+							xtime[r][s][t] = 0;
+						}
+					}
 				}
-			}
-			else { // to check if there is any other possibility ? yes, but forget it.
-				// cout << "line 104... other label_head..." << endl;
-				continue;
-			}
 
-			// caculate energy
-			if (adc >= 2 && tdc == 0) {
-				ene = getene(label_tail, adc, eff);
-				h->Fill(ene);
-			}
+				for (int m = 0; m < 2; m++) {
+					for (int n = 0; n < 2; n++) {
+						tmptimestamp[m][n] = 0;
+					}
+				}
 
-			adc = 0;
-			tdc = 0;
+				for (int s = 0; s < 9; s++) {
+					eaddback[s] = 0;
+					taddback[s] = 0;
+				}
+
+				label_head = *pb >> 12; // for example, "c06b" get c
+				label_tail = *pb & 0x0FFF; // get label
+
+				if (label_head == 8) { // jump useless data // if is ok? shall we need while...
+					pb+=4; // we don't need continue here, because maybe there is a 8440 in an event...
+				}
+				else { // read some information, included adc, tdc, timestamp
+					getadc_tdc_timestamp(pb, label_tail, adc, tdc, etime, xtime);
+					tmptimestamp[0][0] = *(pb-3); // 0-15bit
+					tmptimestamp[0][1] = *(pb-2); // 16-32bit
+					t0 = (tmptimestamp[0][1]<<16) + tmptimestamp[0][0];
+					// tmptimestampcount++;
+					pb+=4;
+				}
+
+				while (1) { // read next fired. still in this event...
+					
+					label_head = *pb >> 12;
+					label_tail = *pb & 0x0FFF;
+					if (label_head == 8) {
+						// cout << "... meet 8 ..." << endl;
+						pb+=4; 
+					}
+					else {
+						tmptimestamp[1][0] = *(pb-3);
+						tmptimestamp[1][1] = *(pb-2);
+
+						// for (int m = 0; m < 2; m++) {
+						// 	for (int n = 0; n < 2; n++) {
+						// 		printf("%04x  ", tmptimestamp[m][n]);
+						// 	}
+						// 	printf("\n");
+						// }
+
+						t1 = (tmptimestamp[1][1]<<16) + tmptimestamp[1][0];
+						// cout << t0 << endl;
+						// cout << t1 << endl;
+						// cout << abs(t0-t1) << "......" << endl;
+
+ 						if (abs((int)(t0 - t1)) >= timeciondience) {
+							eventflag = 0;
+							kk++;
+							break;
+						}
+
+						getadc_tdc_timestamp(pb, label_tail, adc, tdc, etime, xtime);
+						// tmptimestampcount++;
+						pb+=4;
+					}
+				} // after here, event read done.
+
+
+				// add back and energy cal
+				addback(calpar, adc, tdc, etime, eaddback, taddback);
+
+				// for (int s = 0; s < 9; s++) {
+				// 	cout << eaddback[s] << "  ";
+				// }
+				// cout << "......" << endl;
+				
+				// getspecture
+				for (int s = 0; s < 9; s++) {
+					if ((eaddback[s] >= 30) && (eaddback[s] <= 2047)) {
+						hspec->Fill(eaddback[s]);
+					}
+				}
+
+				for (int m = 0; m < 9; m++) {
+					if ((eaddback[m] >= 30) && (eaddback[m] <= 2047)) { // ((eaddback[m] >= 30) && (eaddback[m] <= 2047))
+						firecount++;
+					}
+				}
+
+				hmultifired->Fill(firecount);
+
+				if (firecount == 0) {
+					continue; // jump the event without clover fired...
+				}
+				
+
+				int firedclvnum[9] = {0};
+				int ss = 0; // temp for fireclover
+				for (int s = 0; s < 9; s++) {
+					if ((eaddback[s] >= 30) && (eaddback[s] <= 2047)) {
+						firedclvnum[ss] = s;
+						ss++;
+					}
+				}
+				
+				long long timediff[8] = {0};
+
+				
+				if (firecount >= 2) { // for time difference
+					for (int s = 0; s < (firecount-1); s++) {
+						timediff[s] = taddback[firedclvnum[s]] - taddback[firedclvnum[s+1]] + 4000;
+					}
+					timediff[firecount-1] = taddback[firedclvnum[firecount-1]] - taddback[firedclvnum[0]] + 4000;
+					for (int s = 0; s < firecount; s++) {
+						htime->Fill(timediff[s] + ((rand()%999 + 1) / 100.0));
+						// cout << timediff[s] << endl;
+						// cout << (rand()%999 + 1) / 1000.0 << endl; 
+					}
+					timediff[firecount] = {0};
+				}
+				// for (int m = 0; m < 9; m++) { // test adc value
+				// 	for (int n = 0; n < 4; n++) {
+				// 		printf("%04x  ", adc[m][n]);
+				// 	}
+				// 	printf("......\n");
+				// }
+
+				// cout << "+++" << kk << "+++" << endl;
+				// continue;
+
+				// else if (label_head == 12) {
+				// 	timestamp[0] = *(pb-2);
+				// 	timestamp[1] = *(pb-3);
+				// 	tmptime = timestamp[0]<<16 + timestamp[1];
+				// 	if ((((label_tail>>4) & 0x00F)%2) == 0) { // check the label means adc or tdc
+				// 		adc = *(pb-1) & 0x3FFF; 
+				// 	}
+				// 	else {
+				// 		tdc = *(pb-1) & 0x3FFF; 
+				// 	}
+				// }
+				// else { // to jump like e082, because the energy in these fired is zero, only happened in adc channel...
+				// 	// cout << " ... other label_head..." << endl;
+				// 	// printf("%04x  ", *pb);
+				// 	// printf("%04x  ", *(pb-1));
+				// 	// printf("%04x\n", *(pb-3));
+				// 	pb+=4; // jump the tdc channel after it...
+				// 	continue;
+				// }
+
+				// // caculate energy
+				// if (adc > 0 && tdc == 0) { // make sure is the adc file
+				// 	addbackene = getene(label_tail, adc, eff);
+				// 	h->Fill(addbackene);
+				// }
+
+				// adc = 0;
+				// tdc = 0; // important
+			}
+			
 		}
-	}
+	} // while (fread(block, sizeof(unsigned short), blocksize, fp))
 	
 	fclose(fp);
 	prf->Write();
--- spec.h
+++ spec.h
@@ -7,9 +7,9 @@
 using std::ifstream; using std::cin; using std::cout;
 using std::endl;
 
-int geteff(double eff[9][4][3])
+int getcalpar(double calpar[9][4][3])
 {
-	ifstream fp ("~/sadata/d17/eff/r17.txt");	
+	ifstream fp ("../d17/eneparcal/r17.txt");	
 
 	if (!fp) {
 		cout << "can not open the file." << endl;
@@ -23,7 +23,7 @@ int geteff(double eff[9][4][3])
 			for(int j = 0; j < 9; j++) {
 				for (int k = 0; k < 3; k++) {
 					fp >> a[k];
-					eff[j][i][k] = a[k];
+					calpar[j][i][k] = a[k];
 				}
 			}
 		}
@@ -32,75 +32,350 @@ int geteff(double eff[9][4][3])
 }
 
 
-double getene(unsigned short labei_tail, unsigned short adc, double eff[9][4][3]) 
+void getadc_tdc_timestamp(unsigned short *pb, unsigned short labei_tail, unsigned short adc[9][4], unsigned short tdc[9][4], long long etime[9][4][2], long long xtime[9][4][2]) 
 {
-	double ene = 0;
-	unsigned short clonum = 0;
+	unsigned short clvnum = 0;
 	unsigned short segnum = 0;
 
 	// get clover number
 	switch (labei_tail) {
-		case 64 :
-		case 65 :
-		case 66 :
-		case 67 : clonum = 0;
+		case 0x40 :
+		case 0x41 :
+		case 0x42 :
+		case 0x43 :
+		case 0x50 :
+		case 0x51 :
+		case 0x52 :
+		case 0x53 : clvnum = 0;
 			break;
 
-		case 72 :
-		case 73 :
-		case 74 :
-		case 75 : clonum = 1;
+		case 0x48 :
+		case 0x49 :
+		case 0x4a :
+		case 0x4b :
+		case 0x58 :
+		case 0x59 :
+		case 0x5a :
+		case 0x5b : clvnum = 1;
 			break;
 
-		case 96 :
-		case 97 :
-		case 98 :
-		case 99 : clonum = 2;
+		case 0x60 :
+		case 0x61 :
+		case 0x62 :
+		case 0x63 :
+		case 0x70 :
+		case 0x71 :
+		case 0x72 :
+		case 0x73 : clvnum = 2;
 			break;
 
-		case 104 :
-		case 105 :
-		case 106 :
-		case 107 : clonum = 3;
+		case 0x68 :
+		case 0x69 :
+		case 0x6a :
+		case 0x6b :
+		case 0x78 :
+		case 0x79 :
+		case 0x7a :
+		case 0x7b : clvnum = 3;
 			break;
 
-		case 128 :
-		case 129 :
-		case 130 :
-		case 131 : clonum = 4;
+		case 0x80 :
+		case 0x81 :
+		case 0x82 :
+		case 0x83 :
+		case 0x90 :
+		case 0x91 :
+		case 0x92 :
+		case 0x93 : clvnum = 4;
 			break;
 
-		case 136 :
-		case 137 :
-		case 138 :
-		case 139 : clonum = 5;
+		case 0x88 :
+		case 0x89 :
+		case 0x8a :
+		case 0x8b :
+		case 0x98 :
+		case 0x99 :
+		case 0x9a :
+		case 0x9b : clvnum = 5;
 			break;
 
-		case 160 :
-		case 161 :
-		case 162 :
-		case 163 : clonum = 6;
+		case 0xa0 :
+		case 0xa1 :
+		case 0xa2 :
+		case 0xa3 :
+		case 0xb0 :
+		case 0xb1 :
+		case 0xb2 :
+		case 0xb3 : clvnum = 6; // this is a broken clover...
 			break;
 
-		case 168 :
-		case 169 :
-		case 170 :
-		case 171 : clonum = 7;
+		case 0xa8 :
+		case 0xa9 :
+		case 0xaa :
+		case 0xab :
+		case 0xb8 :
+		case 0xb9 :
+		case 0xba :
+		case 0xbb : clvnum = 7;
 			break;
 
-		case 192 :
-		case 193 :
-		case 194 :
-		case 195 : clonum = 8;
+		case 0xc0 :
+		case 0xc1 :
+		case 0xc2 :
+		case 0xc3 :
+		case 0xd0 :
+		case 0xd1 :
+		case 0xd2 :
+		case 0xd3 : clvnum = 8;
 			break;
 
-		default : cout << "something wrong in switch..." << endl;
+		default : 
+				// cout << "something wrong in switch..." << endl;
+				// printf("%04x  ", *pb);
+				// printf("%04x  ", *(pb-3));
+				// printf("\n");
 			break;
 	}
 
 	// get segnum
-	segnum = (labei_tail&0x000F) % 4;
+	segnum = (labei_tail&0x000F) % 4; // 0, 1, 2, 3 corresponding to a, b, c, d
+	if ((((labei_tail>>4) & 0x00F)%2) == 0) {
+		adc[clvnum][segnum] = *(pb-1) & 0x3FFF;
+		etime[clvnum][segnum][0] = *(pb-3); // 0-15bit
+		etime[clvnum][segnum][1] = *(pb-2); // 16-31bit
+	}  
+	else {
+		tdc[clvnum][segnum] = *(pb-3) & 0x3FFF;
+		xtime[clvnum][segnum][0] = *(pb-3);
+		xtime[clvnum][segnum][1] = *(pb-2);
+	}
+	
+}
 
-	ene = eff[clonum][segnum][0] + adc * eff[clonum][segnum][1] + adc * adc * eff[clonum][segnum][2];
-	return 2 * ene;
+
+void addback(double calpar[9][4][3], unsigned short adc[9][4], unsigned short tdc[9][4], long long etime[9][4][2], double eaddback[9], long long taddback[9])
+{
+	double cyle[9][4];
+	long long cylt[9][4];
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 4; j++) {
+			cyle[i][j] = 0.0;
+			cylt[i][j] = 0;
+		}
+	}
+
+	double random = (rand()%999 + 1) / 1000.0;
+	for (int i = 0; i < 8; i++) {
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][1]<=0) && (adc[i][2]<=0) && (adc[i][3]<=0)) { // 0
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2]; // calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+
+			eaddback[i] = cyle[i][0];
+			taddback[i] = cylt[i][0];
+		}
+
+		if ((adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][0]<=0) && (adc[i][2]<=0) && (adc[i][3]<=0)) { // 1
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+
+			eaddback[i] = cyle[i][1];
+			taddback[i] = cylt[i][1];
+		}
+
+		if ((adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][0]<=0) && (adc[i][1]<=0) && (adc[i][3]<=0)) { // 2
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+
+			eaddback[i] = cyle[i][2];
+			taddback[i] = cylt[i][2];
+		}
+
+		if ((adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][0]<=0) && (adc[i][1]<=0) && (adc[i][2]<=0)) { // 3
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][3];
+			taddback[i] = cylt[i][3];
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][2]<=0) && (adc[i][3]<=0)) { // 0+1
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2];// calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][1];
+			if (cyle[i][0] > cyle[i][1]) {
+				taddback[i] = cylt[i][0];
+			}
+			else {
+				taddback[i] = cylt[i][1];
+			}
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][1]<=0) && (adc[i][3]<=0)) { // 0+2
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2]; // calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][2];
+			if (cyle[i][0] > cyle[i][2]) {
+				taddback[i] = cylt[i][0];
+			}
+			else {
+				taddback[i] = cylt[i][2];
+			}
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][1]<=0) && (adc[i][2]<=0)) { // 0+3
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2]; // calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][3];
+			if (cyle[i][0] > cyle[i][3]) {
+				taddback[i] = cylt[i][0];
+			}
+			else {
+				taddback[i] = cylt[i][3];
+			}
+		}
+
+		if ((adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][0]<=0) && (adc[i][3]<=0)) { // 1+2
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+
+			eaddback[i] = cyle[i][1] + cyle[i][2];
+			if (cyle[i][1] > cyle[i][2]) {
+				taddback[i] = cylt[i][1];
+			}
+			else {
+				taddback[i] = cylt[i][2];
+			}
+		}
+
+		if ((adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][0]<=0) && (adc[i][2]<=0)) { // 1+3
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][1] + cyle[i][3];
+			if (cyle[i][1] > cyle[i][3]) {
+				taddback[i] = cylt[i][1];
+			}
+			else {
+				taddback[i] = cylt[i][3];
+			}
+		}
+
+		if ((adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][0]<=0) && (adc[i][1]<=0)) { // 2+3
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][2] + cyle[i][3];
+			if (cyle[i][2] > cyle[i][3]) {
+				taddback[i] = cylt[i][2];
+			}
+			else {
+				taddback[i] = cylt[i][3];
+			}
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][3]<=0)) { // 0+1+2
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2];// calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][1] + cyle[i][2];
+			if (cyle[i][0] > cyle[i][1] && cyle[i][0] > cyle[i][2]) {
+				taddback[i] = cylt[i][0];
+			}
+			else {
+				taddback[i] = ((cyle[i][1] > cyle[i][2]) ? cyle[i][1] : cyle[i][2]);
+			}
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][2]<=0)) { // 0+1+3
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2];// calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][1] + cyle[i][3];
+			if (cyle[i][0] > cyle[i][1] && cyle[i][0] > cyle[i][3]) {
+				taddback[i] = cylt[i][0];
+			}
+			else {
+				taddback[i] = ((cyle[i][1] > cyle[i][3]) ? cyle[i][1] : cyle[i][3]);
+			}
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][1]<=0)) { // 0+2+3
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2];// calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][2] + cyle[i][3];
+			if (cyle[i][0] > cyle[i][2] && cyle[i][0] > cyle[i][3]) {
+				taddback[i] = cylt[i][0];
+			}
+			else {
+				taddback[i] = ((cyle[i][2] > cyle[i][3]) ? cyle[i][2] : cyle[i][3]);
+			}
+		}
+
+		if ((adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF) && (adc[i][0]<=0)) { // 1+2+3
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2];// calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][1] + cyle[i][2] + cyle[i][3];
+			if (cyle[i][1] > cyle[i][2] && cyle[i][1] > cyle[i][3]) {
+				taddback[i] = cylt[i][1];
+			}
+			else {
+				taddback[i] = ((cyle[i][2] > cyle[i][3]) ? cyle[i][2] : cyle[i][3]);
+			}
+		}
+
+		if ((adc[i][0]>0) && (adc[i][0]<0xFFFF) && (adc[i][1]>0) && (adc[i][1]<0xFFFF) && (adc[i][2]>0) && (adc[i][2]<0xFFFF) && (adc[i][3]>0) && (adc[i][3]<0xFFFF)) { // 0+1+2+3
+			cyle[i][0] = calpar[i][0][0] + adc[i][0]*calpar[i][0][1] + adc[i][0]*calpar[i][0][2]*calpar[i][0][2];// calculation
+			cylt[i][0] = 10 * ((etime[i][0][1]<<16) + (etime[i][0][0])) - 5 + 10 * tdc[i][0] / 65536.0;
+			cyle[i][1] = calpar[i][1][0] + adc[i][1]*calpar[i][1][1] + adc[i][1]*calpar[i][1][2]*calpar[i][1][2]; // calculation
+			cylt[i][1] = 10 * ((etime[i][1][1]<<16) + (etime[i][1][0])) - 5 + 10 * tdc[i][1] / 65536.0;
+			cyle[i][2] = calpar[i][2][0] + adc[i][2]*calpar[i][2][1] + adc[i][2]*calpar[i][2][2]*calpar[i][2][2]; // calculation
+			cylt[i][2] = 10 * ((etime[i][2][1]<<16) + (etime[i][2][0])) - 5 + 10 * tdc[i][2] / 65536.0;
+			cyle[i][3] = calpar[i][3][0] + adc[i][3]*calpar[i][3][1] + adc[i][3]*calpar[i][3][2]*calpar[i][3][2]; // calculation
+			cylt[i][3] = 10 * ((etime[i][3][1]<<16) + (etime[i][3][0])) - 5 + 10 * tdc[i][3] / 65536.0;
+
+			eaddback[i] = cyle[i][0] + cyle[i][1] + cyle[i][2] + cyle[i][3];
+			if (cyle[i][0] > cyle[i][1] && cyle[i][0] > cyle[i][2] && cyle[i][0] > cyle[i][3]) {
+				taddback[i] = cylt[i][0];
+			}
+			else if (cyle[i][1] > cyle[i][0] && cyle[i][1] > cyle[i][2] && cyle[i][1] > cyle[i][3]) {
+				taddback[i] = cylt[i][1];
+			}
+			else {
+				taddback[i] = ((cyle[i][2] > cyle[i][3]) ? cyle[i][2] : cyle[i][3]);
+			}
+		}
+	}
 }
